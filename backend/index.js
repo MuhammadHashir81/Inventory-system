6b0dfbc20b0bb8cf92aeefd5a1f4ec3d2521c970
SaifullahUsmani693 <saifullahusmani693@gmail.com>
2025-11-11 15:15:21 +0500

    port changed

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import { soldItemRouter } from './Routes/soldItem.route.js';
 import { debtRouter } from './Routes/debt.route.js';
 configDotenv()
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3005
 
 
 app.use(express.json());
